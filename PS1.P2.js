dbceb720ae77f08de4e40b4493ee7cea9f180dc6
EzechielD <ezechield07@gmail.com>
2022-02-09 12:36:04 -0500

    commented out tests

--- PS1.P2.js
+++ PS1.P2.js
@@ -19,8 +19,8 @@ const p2a = ([left, operator, right]) => {
 };
 
 /* Test */
-console.log(`P2A: ${p2a('8/8')()}`);
-console.log(`P2A: ${p2a('8+9')()}`);
-console.log(`P2A: ${p2a('4*6')()}`);
-console.log(`P2A: ${p2a('2^4')()}`);
-console.log(`P2A: ${p2a('7-3')()}`);
+// console.log(`P2A: ${p2a('8/8')()}`);
+// console.log(`P2A: ${p2a('8+9')()}`);
+// console.log(`P2A: ${p2a('4*6')()}`);
+// console.log(`P2A: ${p2a('2^4')()}`);
+// console.log(`P2A: ${p2a('7-3')()}`);
