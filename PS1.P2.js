4b4eea261c162ff7b05dc6d8e579dfa4bdb84767
EzechielD <ezechield07@gmail.com>
2022-02-09 12:33:49 -0500

    removed expression param

--- PS1.P2.js
+++ PS1.P2.js
@@ -2,25 +2,25 @@ const p2a = ([left, operator, right]) => {
     switch(operator) {
         case '+':
             console.log(`Left: ${left} Op: ${operator} Right: ${right}`);
-            return (expression) => Number(left) + Number(right);
+            return () => Number(left) + Number(right);
         case '-':
             console.log(`Left: ${left} Op: ${operator} Right: ${right}`);
-            return (expression) => Number(left) - Number(right);
+            return () => Number(left) - Number(right);
         case '*':
             console.log(`Left: ${left} Op: ${operator} Right: ${right}`);
-            return (expression) => Number(left) * Number(right);
+            return () => Number(left) * Number(right);
         case '/':
             console.log(`Left: ${left} Op: ${operator} Right: ${right}`);
-            return (expression) => Number(left) / Number(right);
+            return () => Number(left) / Number(right);
         case '^':
             console.log(`Left: ${left} Op: ${operator} Right: ${right}`);
-            return (expression) => Number(left) ** Number(right);
+            return () => Number(left) ** Number(right);
     }
 };
 
 /* Test */
-// console.log(`P2A: ${p2a('8/8')()}`);
-// console.log(`P2A: ${p2a('8+9')()}`);
-// console.log(`P2A: ${p2a('4*6')()}`);
-// console.log(`P2A: ${p2a('2^4')()}`);
-// console.log(`P2A: ${p2a('7-3')()}`);
+console.log(`P2A: ${p2a('8/8')()}`);
+console.log(`P2A: ${p2a('8+9')()}`);
+console.log(`P2A: ${p2a('4*6')()}`);
+console.log(`P2A: ${p2a('2^4')()}`);
+console.log(`P2A: ${p2a('7-3')()}`);
