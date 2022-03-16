29d3aa8ada366dfc671f0acc9e970aa92642176d
EzechielD <ezechield07@gmail.com>
2022-03-16 11:42:29 -0400

    completed PS2

--- ps2/index.js
+++ ps2/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+/* part b */
 router.get('/', (req, res, next) => {
   res.render('index', {dsb: 'Data Science Building' });
 });
 
-/* POST to home page. */
-router.post('/', (req, res, next)  => {
+/* part c */
+router.post('/post', (req, res, next)  => {
   res.render('index', {cas: req.body.id});
 });
 
+/* part d */
+router.get('/namedParam/:id', (req, res, next) => {
+  let foo;
+});
 
 module.exports = router;
