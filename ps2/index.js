85ef34561c242559177e012f6a0f594cf353181e
EzechielD <ezechield07@gmail.com>
2022-03-16 03:26:53 -0400

    removed routes

--- ps2/index.js
+++ ps2/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', {dsb: 'Data Science Building' });
 });
 
+/* POST to home page. */
+// router.post('/', function(req, res, next) {
+//   res.render('index', {cas: 'College of Arts and Sciences' });
+// });
+
 module.exports = router;
